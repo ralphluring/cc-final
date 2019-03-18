c3f76e482c5968eb345fc64445491b2d65bd5269
Ralph Luring <bennettchrislloyd@gmail.com>
2019-03-17 21:53:32 -0700

    mobile nav popout working

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@ let yesButton = document.getElementById("yes-button");
 let mainPage = document.getElementById("main-wrapper");
 let ageModal = document.getElementById("age-modal");
 let ageLogo = document.getElementById("age-logo");
+let menuButton = document.getElementById("menu-button");
+let nav = document.getElementById("nav");
 let body = document.querySelector("body");
 
 if(sessionStorage.getItem('age')){
@@ -13,19 +15,52 @@ if(sessionStorage.getItem('age')){
 }
 
 yesButton.addEventListener("click",function(){
-    body.style.backgroundColor = "#FFBD0D";
+
     setInterval(function(){
-       
         ageModal.style.display = "none";
         mainPage.classList.add("animated", "fadeIn");
-        
      }, 1000);
 
     ageModal.classList.add("animated","fadeOut");
-    
     mainPage.style.display = "grid";
-    sessionStorage.setItem('age', 'yes');
-    
+    sessionStorage.setItem('age', 'yes');    
 });
+
+let show = false;
+nav.classList.add("nav-hide");
+
+menuButton.addEventListener("click", function(){
+
+    if(show === false){
+        nav.classList.remove("nav-hide");
+        nav.classList.add("nav-pulse-1");
+        nav.classList.add("nav-show");
+        show = true;
+     
+    }else{
+     
+        
+           nav.classList.remove("nav-show");
+            nav.classList.add("nav-hide");
+            show = false;
+    
+        
+
+        
+        
+       
+    }
+    
+ 
+});
+
+
+
+
+
+
+
+
+
 
 
